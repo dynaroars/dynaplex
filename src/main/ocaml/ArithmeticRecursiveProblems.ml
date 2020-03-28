c7133bbb5db303b03895c9a98a0a9094d42c3da3
JIexa <malyshevalexey8@gmail.com>
2020-03-27 20:27:07 -0500

    gcd ocaml

--- src/main/ocaml/ArithmeticRecursiveProblems.ml
+++ src/main/ocaml/ArithmeticRecursiveProblems.ml
@@ -13,18 +13,25 @@ let range k n =
 let drop ls n = 
     let rec aux i acc = function
         | [] -> acc
-        | h :: t -> if i=n then aux 1 acc t else aux (i+1) (h :: acc) t
+        | h :: t -> begin
+            (* find element that we should start with dropping each nth element *)
+            if n>=h then aux 1 (h::acc) t else
+            if i=n then aux 1 acc t else aux (i+1) (h :: acc) t
+        end
     in List.rev(aux 0 [] ls)
 ;;
 
-let rec find_p p = function
+let rec find_next_p p = function
     | [] -> -1
-    | h :: t -> if h >= p then h else find_p p t
+    | h :: t -> if h >= p then h else find_next_p p t
 ;;
 
+(* TODO: fix it *)
 let rec get_primes ls p = 
-    let new_p = find_p p ls in
+    let new_p = find_next_p p ls in
     printf "p: %d\n" new_p;
+    List.iter (printf "%d ") ls;
+    printf "\n";
     if new_p = -1 then 
         ls 
     else begin
@@ -46,9 +53,22 @@ let is_prime num =
     
 ;;
 
+let gcd a b = 
+    let smallest_num, biggest_num = 
+        if a <= b then a, b else b, a
+    in
+    let rec aux a b =
+        if a == 0 then b else
+            aux (b mod a) a 
+    in aux smallest_num biggest_num
+;;
+
 
 let main = begin
-    is_prime 30;
+    (*is_prime 30;*)
+
+    let d = gcd 20536 7826 in
+    printf "%d\n" d;
 
 end;;
 
