760e59f014fd05dfb356c2c34ff7bbe168a55e87
Didier Ishimwe <ishimwed@huskers.unl.edu>
2020-11-14 22:20:50 -0600

    modified

--- recursive_programs/closest_pair.py
+++ recursive_programs/closest_pair.py
@@ -1,44 +1,48 @@
 #!/usr/bin/env python3
+
 from __future__ import division
 from collections import namedtuple
 from random import randint
 import math as m
 import heapq as hq
+import random
 import os
-# import random
 
 counter = 0
 
 def get_key(item):
     return(item[0])
 
+
 def closest_point_problem(points, depth, file):
     global counter
-
     point = []
     heap = []
     pt = namedtuple('pt', 'x y')
     for i in range(len(points)):
         counter = counter + 1
         point.append(pt(points[i][0], points[i][1]))
-    point = sorted(point, key=get_key)                 #this line cause dig to fail
+    point = sorted(point, key=get_key)
     visited_index = []
     find_min(0, len(point) - 1, point, heap, visited_index, depth, file)
     # print(hq.heappop(heap))
 
 def find_min(start, end, point, heap, visited_index, depth, file):
+    global counter
     with open(file, 'a') as f:
-        print("{};2;{}".format(depth, end-start+1), file=f)
-
+        print("{};2;{}".format(depth, len(point[start:end + 1])), file=f)
     if len(point[start:end + 1]) & 1:
         mid = start + ((len(point[start:end + 1]) + 1) >> 1)
+        counter = counter + 1
     else:
+        counter = counter + 1
         mid = start + (len(point[start:end + 1]) >> 1)
         if start in visited_index:
             start = start + 1
         if end in visited_index:
             end = end - 1
     if len(point[start:end + 1]) > 3:
+        counter = counter + 1
         if start < mid - 1:
             distance1 = m.sqrt((point[start].x - point[start + 1].x) ** 2 + (point[start].y - point[start + 1].y) ** 2)
             distance2 = m.sqrt((point[mid].x - point[mid - 1].x) ** 2 + (point[mid].y - point[mid - 1].y) ** 2)
@@ -66,17 +70,20 @@ def find_min(start, end, point, heap, visited_index, depth, file):
 
 if __name__ == '__main__':
     for i in range(100):
-        x = []
-        num_points = randint(2, 150)
-        path = "./closest_pair"
+        points = []
+        depth = 0
+        num_points = random.randint(10,100)
+        for i in range(num_points):
+            points.append((randint(- num_points << 2, num_points << 2), randint(- num_points << 2, num_points << 2)))
+
+        path = "./closest"
         try:
             os.mkdir(path)
         except OSError as error:
             pass
-        for i in range(num_points):
-            x.append((randint(- num_points << 2, num_points << 2), randint(- num_points << 2, num_points << 2)))
-        file = "./closest_pair/output-{}".format(len(x))
-        closest_point_problem(x, 0, file)
-        with open("./closest_pair/traces", 'a') as f:
-            print("{};{}".format(len(x), counter), file=f)
+        file = "./closest/output-{}".format(num_points)
+        closest_point_problem(points, depth, file)
+
+        with open("./closest/traces", 'a') as f:
+            print("{};{}".format(num_points, counter), file=f)
         counter = 0
