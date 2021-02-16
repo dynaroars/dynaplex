be17fc5cd5022122b625ad0f40e91bc7bee64186
KSH <ksh@ksh.com>
2021-02-16 17:18:54 -0600

    convexhull

--- recursive_programs/alg3/gen.sh
+++ recursive_programs/alg3/gen.sh
@@ -11,3 +11,5 @@ N=100
 ./gen_traces.sh "permutation_index" $N 1 100
 GEN_SEQUENTIAL=1 ./gen_traces.sh "permutation_gen" _ 1 19
 GEN_SEQUENTIAL=1 ./gen_traces.sh "tenary_012" _ 1 12
+
+./gen_traces.sh "convexhull" $N 1 1000
