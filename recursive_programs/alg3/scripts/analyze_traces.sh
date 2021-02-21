6afbe270048f2ee7e93a79cd37c87ef669f76308
KSH <ksh@ksh.com>
2021-02-21 12:42:15 -0600

    upd analy

--- recursive_programs/alg3/scripts/analyze_traces.sh
+++ recursive_programs/alg3/scripts/analyze_traces.sh
@@ -4,15 +4,22 @@ set -euo pipefail
 NAME=$1
 ADIR=res/
 RES=$ADIR/$NAME
+AOUT=$ADIR/analyzer_out
 TDIR=traces/$NAME/
 
 mkdir -p "$ADIR"
 
-SEED=$(cat $TDIR/_seed)
-printf "SEED=$SEED  " >> $RES
 
-../../analyzer.py -trace $TDIR \
-    | tee /dev/stderr \
+../../analyzer.py -trace $TDIR 2>&1 | tee $AOUT
+
+SEED=$(cat $TDIR/_seed)
+printf "SEED=$SEED\t " >> $RES
+cat $AOUT \
     | grep "b'Complexity is " \
     | sed -n -e 's/^b'"'"'Complexity is \(.*\)\\n'"'"'$/\1/p' \
+    >> $RES
+
+printf "SEED=$SEED\t " >> $RES
+cat $AOUT \
+    | grep "T(n) = " \
     >> $RES
