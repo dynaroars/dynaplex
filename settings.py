39225f0c66f27b51093c89d86ee60c08d80dbc7b
KSH <ksh@ksh.com>
2021-02-21 19:08:22 -0600

    upd src

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
-path_to_src = "/home/dishimwe/dishimwe/complexity-recurrence"
+import os
+
+path_to_src = os.path.dirname(os.path.abspath(__file__))
 maxdeg = 5
 plot = False
 min_freq = 0.95 #this is the frequency used as heuristics to select between T(n-a) and T(n/a)
