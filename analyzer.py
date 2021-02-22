6eed0b8290813999e3207edf05a1412e814e2575
Didier Ishimwe <ishimwed@huskers.unl.edu>
2021-02-21 18:41:21 -0600

    analyzer

--- analyzer.py
+++ analyzer.py
@@ -5,11 +5,9 @@ from os import listdir, path
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn import preprocessing, svm
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.cluster import KMeans
-from sklearn.metrics import r2_score
 import math
 import time
 import argparse
