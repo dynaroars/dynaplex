cc0a089820c3e5e6425996c260781815675f1bbe
KSH <ksh@ksh.com>
2021-04-12 15:43:41 -0500

    upd alg3 params

--- recursive_programs/alg3/rand.cpp
+++ recursive_programs/alg3/rand.cpp
@@ -12,7 +12,11 @@ int main(int argc, char *argv[]) {
     ll to = atoll(argv[2]);
     int cnt = (argc >= 4 ? atoi(argv[3]) : 1);
 
-    std::vector<ll> res = rnd.distinct(cnt, from, to);
+    //std::vector<ll> res = rnd.distinct(cnt, from, to);
+    std::vector<ll> res; res.reserve(cnt);
+    for(int i = 0; i < cnt; i++) {
+        res.push_back(rnd.next(from, to));
+    }
     std::sort(res.begin(), res.end());
 
     for(auto v : res)
--- recursive_programs/alg3/scripts/gen_all.sh
+++ recursive_programs/alg3/scripts/gen_all.sh
@@ -9,11 +9,11 @@ echo "GenAll - seed='${GEN_SEED:-}'"
 ./scripts/gen_traces.sh "mergetree_build" $N 1 500
 ./scripts/gen_traces.sh "mergetree_mem" $N 1 500
 ./scripts/gen_traces.sh "mergetree_query" $N 1 500
-./scripts/gen_traces.sh "powbin" $N 0 $I64MAX
+./scripts/gen_traces.sh "powbin" $N 0 500
 ./scripts/gen_traces.sh "pancakesort" $N 1 500
 ./scripts/gen_traces.sh "quickselect" $N 1 500
 ./scripts/gen_traces.sh "permutation_index" $N 1 500
-./scripts/gen_traces.sh "permutation_gen" 10 1 20
-./scripts/gen_traces.sh "tenary_012" 10 1 12
+./scripts/gen_traces.sh "permutation_gen" $N 1 20
+./scripts/gen_traces.sh "tenary_012" $N 1 12
 ./scripts/gen_traces.sh "convexhull" $N 1 500
-./scripts/gen_traces.sh "balanced_ternary" $N 0 $I64MAX
+./scripts/gen_traces.sh "balanced_ternary" $N 0 500
