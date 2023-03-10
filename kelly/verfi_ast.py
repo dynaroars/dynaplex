54620a3fd828362721d9bfd6b4b32ad2c41929a4
kelluhubert <irakellyhub@gmail.com>
2023-03-10 11:55:29 +0200

    prints depth function

---
+++ kelly/verfi_ast.py
@@ -0,0 +1,32 @@
+
+import ast
+
+def depth(statement):
+    tree = ast.parse(statement)
+
+    class add_depth(ast.NodeTransformer):
+        def visit_FunctionDef(self, node):
+            # Add a new argument 'depth' to the function definition
+            node.args.args.append(ast.arg(arg='depth', annotation=None))
+
+            # Add the 'depth' argument to all function calls inside the function body
+            for subnode in ast.walk(node):
+                if isinstance(subnode, ast.Call) and isinstance(subnode.func, ast.Name) and subnode.func.id == node.name:
+                    subnode.args.append(ast.Name(id='depth', ctx=ast.Load()))
+
+            return self.generic_visit(node)
+
+    update_tree = add_depth().visit(tree)
+    update_code = ast.unparse(update_tree)
+    return update_code
+
+code = """
+def rec_foo(x):
+    if not x > verifier_nondet_int():
+        return 0
+    x -= 1
+    return rec_foo(x)
+"""
+
+update_code = depth(code)
+print(update_code)
