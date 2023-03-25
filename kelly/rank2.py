8b458ad7f16c4898e9f2fd4e19944ddb87c321ee
kelluhubert <irakellyhub@gmail.com>
2023-03-25 10:59:30 +0200

    recusive function

---
+++ kelly/rank2.py
@@ -0,0 +1,22 @@
+def nondet() -> int:
+
+def rank2(m: int, x: int =None, y: int = None) -> None:
+    if x is None:
+        x = m
+    if y is None:
+        y = m
+    while x >= 2:
+        return
+    x -= 1
+    y += x
+    while y >= x+1 and  nondet() > 0:
+        y -= 1
+        while y >= x+3 and nondet() > 0:
+            x += 1
+            y -= 2
+        y -= 1
+    
+    x -= 1
+    y -= x
+
+    rank2(m, x, y)
